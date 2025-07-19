8de0608b34c096a33db006141a63387ef1e1a007
p-brycko <221553333+p-brycko@users.noreply.github.com>
2025-07-19 10:55:38 +0200

    Move card actions registration to an extension method

--- MadiffInterview/CardActionsServiceCollectionExtensions.cs
+++ MadiffInterview/CardActionsServiceCollectionExtensions.cs
@@ -4,6 +4,83 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class CardActionsServiceCollectionExtensions
     {
+        public static IServiceCollection RegisterCardActions(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            // actions and their conditions provided in the task's document
+            services.RegisterCardAction("ACTION1", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Active));
+
+            services.RegisterCardAction("ACTION2", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Inactive));
+
+            services.RegisterCardAction("ACTION3", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowAllCardStatuses());
+
+            services.RegisterCardAction("ACTION4", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowAllCardStatuses());
+
+            services.RegisterCardAction("ACTION5", conditions => conditions
+                .AllowCardType(CardType.Credit)
+                .AllowAllCardStatuses());
+
+            services.RegisterCardAction("ACTION6", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered, card => card.IsPinSet)
+                .AllowCardStatus(CardStatus.Inactive, card => card.IsPinSet)
+                .AllowCardStatus(CardStatus.Active, card => card.IsPinSet)
+                .AllowCardStatus(CardStatus.Blocked, card => card.IsPinSet));
+
+            services.RegisterCardAction("ACTION7", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered, card => !card.IsPinSet)
+                .AllowCardStatus(CardStatus.Inactive, card => !card.IsPinSet)
+                .AllowCardStatus(CardStatus.Active, card => !card.IsPinSet)
+                .AllowCardStatus(CardStatus.Blocked, card => card.IsPinSet));
+
+            services.RegisterCardAction("ACTION8", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered)
+                .AllowCardStatus(CardStatus.Inactive)
+                .AllowCardStatus(CardStatus.Active)
+                .AllowCardStatus(CardStatus.Blocked));
+
+            services.RegisterCardAction("ACTION9", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowAllCardStatuses());
+
+            services.RegisterCardAction("ACTION10", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered)
+                .AllowCardStatus(CardStatus.Inactive)
+                .AllowCardStatus(CardStatus.Active));
+
+            services.RegisterCardAction("ACTION11", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered)
+                .AllowCardStatus(CardStatus.Inactive)
+                .AllowCardStatus(CardStatus.Active));
+
+            services.RegisterCardAction("ACTION12", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered)
+                .AllowCardStatus(CardStatus.Inactive)
+                .AllowCardStatus(CardStatus.Active));
+
+            services.RegisterCardAction("ACTION13", conditions => conditions
+                .AllowAllCardTypes()
+                .AllowCardStatus(CardStatus.Ordered)
+                .AllowCardStatus(CardStatus.Inactive)
+                .AllowCardStatus(CardStatus.Active));
+
+            return services;
+        }
+
         public static IServiceCollection RegisterCardAction(this IServiceCollection services, string actionName, Action<CardActionConditions> conditionsBuilder)
         {
             ArgumentNullException.ThrowIfNull(services);
--- MadiffInterview/Program.cs
+++ MadiffInterview/Program.cs
@@ -9,7 +9,6 @@ namespace MadiffInterview
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
             ConfigureServices(builder.Services);
-            RegisterCardActions(builder.Services);
 
             WebApplication app = builder.Build();
 
@@ -37,80 +36,9 @@ namespace MadiffInterview
         {
             services.AddLogging();
             services.AddSingleton<CardService>();
+
             services.AddTransient<CardActionsService>();
-        }
-
-        private static void RegisterCardActions(IServiceCollection services)
-        {
-            // actions and their conditions provided in the task's document
-            services.RegisterCardAction("ACTION1", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Active));
-
-            services.RegisterCardAction("ACTION2", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Inactive));
-
-            services.RegisterCardAction("ACTION3", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowAllCardStatuses());
-
-            services.RegisterCardAction("ACTION4", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowAllCardStatuses());
-
-            services.RegisterCardAction("ACTION5", conditions => conditions
-                .AllowCardType(CardType.Credit)
-                .AllowAllCardStatuses());
-
-            services.RegisterCardAction("ACTION6", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered, card => card.IsPinSet)
-                .AllowCardStatus(CardStatus.Inactive, card => card.IsPinSet)
-                .AllowCardStatus(CardStatus.Active, card => card.IsPinSet)
-                .AllowCardStatus(CardStatus.Blocked, card => card.IsPinSet));
-
-            services.RegisterCardAction("ACTION7", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered, card => !card.IsPinSet)
-                .AllowCardStatus(CardStatus.Inactive, card => !card.IsPinSet)
-                .AllowCardStatus(CardStatus.Active, card => !card.IsPinSet)
-                .AllowCardStatus(CardStatus.Blocked, card => card.IsPinSet));
-
-            services.RegisterCardAction("ACTION8", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered)
-                .AllowCardStatus(CardStatus.Inactive)
-                .AllowCardStatus(CardStatus.Active)
-                .AllowCardStatus(CardStatus.Blocked));
-
-            services.RegisterCardAction("ACTION9", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowAllCardStatuses());
-
-            services.RegisterCardAction("ACTION10", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered)
-                .AllowCardStatus(CardStatus.Inactive)
-                .AllowCardStatus(CardStatus.Active));
-
-            services.RegisterCardAction("ACTION11", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered)
-                .AllowCardStatus(CardStatus.Inactive)
-                .AllowCardStatus(CardStatus.Active));
-
-            services.RegisterCardAction("ACTION12", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered)
-                .AllowCardStatus(CardStatus.Inactive)
-                .AllowCardStatus(CardStatus.Active));
-
-            services.RegisterCardAction("ACTION13", conditions => conditions
-                .AllowAllCardTypes()
-                .AllowCardStatus(CardStatus.Ordered)
-                .AllowCardStatus(CardStatus.Inactive)
-                .AllowCardStatus(CardStatus.Active));
+            services.RegisterCardActions();
         }
     }
 }
